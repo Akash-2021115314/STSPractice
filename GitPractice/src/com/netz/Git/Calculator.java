0afeeedc1d7765deaa19f497565197f48ccf0d7a
ThenrajM <146869626+ThenrajM@users.noreply.github.com>
2026-03-23 18:51:35 +0530

    Fix addition logic in Calculator.java
    Corrected addition logic and removed invalid operation.

--- GitPractice/src/com/netz/Git/Calculator.java
+++ GitPractice/src/com/netz/Git/Calculator.java
@@ -18,7 +18,7 @@ public class Calculator {
         int sub = a - b;
         int mul = a * b;
         int div = a / b;
-        int put = a $ b;
+        int add = b + c;
         System.out.println("Addition: " + add);
         System.out.println("Subtraction: " + sub);
         System.out.println("Multiplication: " + mul);
